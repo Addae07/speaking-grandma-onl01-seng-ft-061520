ed252839d4a1038af21a6be589f0a27a4028f4d5
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-20 19:40:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,14 +1,4 @@
 def speak_to_grandma(phrase)
-  if phrase != phrase.uppercase()
-return "HUH?! SPEAK UP, SONNY!"# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
- else
-    return "NO, NOT SINCE 1938!"
-  end
-end
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
-def speak_to_grandma(phrase)
   if(phrase=="I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
   elsif (phrase=="#{phrase}".upcase)
